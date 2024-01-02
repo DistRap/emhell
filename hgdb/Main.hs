0b1c8c8adeed4b0f37456ad4940f004d1b8b2b74
sorki <srk@48.io>
2024-01-02 17:51:45 +0100

    hgdb: add :set for setting register values

--- hgdb/Main.hs
+++ hgdb/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
@@ -18,17 +19,24 @@ import System.Console.Repline
   , CompleterStyle(Prefix)
   , ExitDecision(Exit)
   )
+import Prettyprinter ((<+>), annotate, pretty)
+import Prettyprinter.Render.Terminal (Color(..), bold, color)
 
 import qualified Control.Concurrent
 import qualified Control.Exception
 import qualified Control.Monad
+import qualified Data.Bits.Pretty
 import qualified Data.SVD.IO
 import qualified Data.SVD.Pretty.Explore
+import qualified Data.Text.IO
 import qualified EmHell.SigintHandler
 import qualified EmHell.SVD.Completion
 import qualified EmHell.SVD.Query
+import qualified EmHell.SVD.Manipulation
 import qualified EmHell.SVD.Selector
 import qualified Gdb
+import qualified Prettyprinter
+import qualified Prettyprinter.Render.Terminal
 import qualified System.Console.Repline
 import qualified System.Directory
 
@@ -111,7 +119,8 @@ runRepl = do
 
     options :: [(String, String -> Repl ())]
     options = [
-        ("svd", loadSVD)
+        ("set", setReg)
+      , ("svd", loadSVD)
       , ("file", liftGdb . Gdb.file)
       , ("wait", wait)
       , ("c", interruptible $ Gdb.continue >> Gdb.waitStop)
@@ -136,9 +145,24 @@ runRepl = do
         Just dev ->
           EmHell.SVD.Completion.svdCompleter dev x
 
+    svdCompleterFieldsMay
+      :: Monad m
+      => String
+      -> StateT (Maybe Device) m [String]
+    svdCompleterFieldsMay x = do
+      s <- get
+      case s of
+        Nothing -> pure mempty
+        Just dev ->
+          EmHell.SVD.Completion.svdCompleterFields dev x
+
     defaultMatcher :: [(String, CompletionFunc (StateT (Maybe Device) (GDBT IO)))]
     defaultMatcher =
-      [ (":svd", System.Console.Repline.fileCompleter)
+      [ ( ":set"
+        , EmHell.SVD.Completion.compFunc
+            svdCompleterFieldsMay
+        )
+      , (":svd", System.Console.Repline.fileCompleter)
       , (":file", System.Console.Repline.fileCompleter)
       ]
 
@@ -166,7 +190,11 @@ replCmd input = lift $ do
             of
               Left e -> lift $ Gdb.echo e
               Right (reg, regAddr) -> do
-                res <- lift $ Gdb.readMem regAddr 4
+                res <-
+                  lift
+                    . Gdb.readMem32
+                    . Gdb.memAddr
+                    $ fromIntegral regAddr
                 case res of
                   Nothing ->
                     lift $ Gdb.echo "Failed to read memory via GDB"
@@ -229,3 +257,82 @@ liftGdb
   => m a
   -> t1 (t2 m) a
 liftGdb fn = lift . lift $ fn
+
+setReg :: String -> Repl ()
+setReg input = lift $ do
+  svdMay <- get
+  case svdMay of
+    Nothing -> liftIO $ putStrLn "No SVD loaded"
+    Just dev -> do
+      case EmHell.SVD.Selector.parseSelectorValue input of
+        Left e ->
+          liftIO
+            $ putStrLn
+            $ "No parse " <> e
+        Right (sel, v) ->
+          case
+              EmHell.SVD.Query.getRegWithAddr
+                (selPeriph sel)
+                (selReg sel)
+                dev
+            of
+              Right (reg, regAddr) -> do
+                let regMemAddr =
+                      Gdb.memAddr
+                      $ fromIntegral regAddr
+
+                mOrigRegVal <-
+                  lift $ Gdb.readMem32 regMemAddr
+
+                case mOrigRegVal of
+                  Nothing -> liftIO $ putStrLn "Unable to read memory"
+                  Just origRegVal -> do
+                    let eNewVal = case selField sel of
+                          Just f ->
+                            EmHell.SVD.Manipulation.setField
+                              reg
+                              origRegVal
+                              f
+                              v
+                          Nothing ->
+                            pure v
+                    case eNewVal of
+                      Left e ->
+                        liftIO
+                          . Data.Text.IO.putStrLn
+                          . Prettyprinter.Render.Terminal.renderStrict
+                          $ Prettyprinter.layoutPretty
+                              Prettyprinter.defaultLayoutOptions
+                              e
+                      Right newVal -> do
+                        liftIO
+                          . Data.Text.IO.putStrLn
+                          . Prettyprinter.Render.Terminal.renderStrict
+                          $ Prettyprinter.layoutPretty
+                              Prettyprinter.defaultLayoutOptions
+                              (annotate
+                                (bold <> color Green)
+                                (   "Writing"
+                                <+> pretty (Data.Bits.Pretty.formatHex newVal)
+                                <+> "to"
+                                <+> pretty (selPeriph sel)
+                                <>  "."
+                                <>  pretty (selReg sel)
+                                )
+                              )
+                        lift
+                          $ Gdb.writeMem32
+                              regMemAddr
+                              [newVal]
+
+                        mNewRegVal <- lift $ Gdb.readMem32 regMemAddr
+                        case mNewRegVal of
+                          Nothing -> liftIO $ putStrLn "Unable to re-read memory"
+                          Just newRegVal ->
+                            liftIO
+                              $ Data.SVD.Pretty.Explore.exploreRegister
+                                  newRegVal
+                                  regAddr
+                                  reg
+
+              Left e -> liftIO $ putStrLn e
